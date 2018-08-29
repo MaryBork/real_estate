907d3467f2b63f9f9962634c8019cc1677fec866
Mary Bork <maryebork@outlook.com>
2018-08-29 15:01:22 -0600

    Adds Iteration 3

--- test/house_test.rb
+++ test/house_test.rb
@@ -20,7 +20,7 @@ class HouseTest < Minitest::Test
   def test_it_has_address
     house = House.new("$400000", "123 sugar lane")
 
-    assert_equal "123 sugar lane"
+    assert_equal "123 sugar lane", house.address
   end
 
 
@@ -30,4 +30,55 @@ class HouseTest < Minitest::Test
     assert_equal ([]), house.rooms
   end
 
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+
+    assert_equal house.rooms, house.add_room(room_1)
+    assert_equal house.rooms, house.add_room(room_2)
+    assert_equal 2, house.rooms.count
+  end
+
+  def test_it_can_categorize_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal ([room_1, room_2]), house.rooms_by_category(:bedroom)
+    assert_equal ([room_4]), house.rooms_by_category(:basement)
+  end
+
+  def test_house_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+end
+
+  # def test_price_by_square_foot
+  #   house = House.new("$400000", "123 sugar lane")
+  #   room_1 = Room.new(:bedroom, 10, 13)
+  #   room_2 = Room.new(:bedroom, 11, 15)
+  #   room_3 = Room.new(:living_room, 25, 15)
+  #   room_4 = Room.new(:basement, 30, 41)
+  #   house.add_room(room_1)
+  #   house.add_room(room_2)
+  #   house.add_room(room_3)
+  #   house.add_room(room_4)
+  #
+  #   assert_equal 210.53, house.price_per_square_foot
+  # end 
 end
